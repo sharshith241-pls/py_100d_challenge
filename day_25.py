5ff017c9df6e8841b175501f61208a5876102bc7
sharshith241-svg <sharshith241@gmail.com>
2026-03-30 06:13:10 +0530

    day25

---
+++ day_25.py
@@ -0,0 +1,34 @@
+# countries=("spanish","India","russia","France")
+# temp=list(countries)
+# temp.append("Mongolia")
+# countries=tuple(temp)
+# print(countries)
+###################################################################################################
+# countries=("spanish","India","russia","France")
+# temp=list(countries)
+# temp.pop(2)
+# countries=tuple(temp)
+# print(countries)
+###################################################################################################
+# countries=("spanish","India","russia","France")
+# temp=list(countries)
+# temp[2]="Iraq"
+# countries=tuple(temp)
+# print(countries)
+###################################################################################################
+# countries=("spanish","India","russia","France")
+# countries2=('Iraq','Mongolia')
+# countries3=countries+countries2
+# print(countries3)
+###################################################################################################
+# tuple1=(0,1,2,3,2,3,1,3,2,4)
+# res=tuple1.count(3)
+# print('Count of 3 in tuple1 is :',3)
+###################################################################################################
+# tuple1=(0,1,2,3,2,3,1,3,2,4)
+# res=tuple1.index(3,4,7)
+# print("First occurence at position :",res)
+###################################################################################################
+tuple1=(0,1,2,3,2,3,1,3,2,4)
+res=len(tuple1)
+print("Length of the tuple :",res)
