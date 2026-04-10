9737ea5d75da8a01c819cbbc64f9a25ef59a46a6
Harshith <sharshith241@gmail.com>
2026-04-10 12:40:30 +0530

    day_29

---
+++ day_29.py
@@ -0,0 +1,18 @@
+# letter="Hey my name is  {} and I am from {}"
+# country="India"
+# name="Harry"
+# print(letter.format(name,country))
+# print(f"Hey my name is {name} and i am in {country}")
+###################################################################################################
+# txt=f"For only {price:.2f} dollars!"
+# print(txt.format(price=49.99999))
+###################################################################################################
+# price=56.0838383983
+# txt=f"For only {price:.2f} dollars!"
+# # print(txt.format(price=49.99999))
+# print(txt)
+###################################################################################################
+# print(type(f"2*30"))
+###################################################################################################
+print(f"Hey my name is {{name}} and i am in {{country}}")
+
