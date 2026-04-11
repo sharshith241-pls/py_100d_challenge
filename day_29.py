e0ab27335711aa49da1f234edae79b052f21dfcd
Harshith <sharshith241@gmail.com>
2026-04-11 06:02:15 +0530

    day_29

---
+++ day_29.py
@@ -0,0 +1,19 @@
+# def square(n):
+#     print(n**2)
+# square(6)
+###################################################################################################
+# just an random example
+# n = int(input("Enter the number: "))
+
+# k = 0
+
+# for i in range(n, 0, -1):   
+#     k=k+i
+
+# print(k)
+###################################################################################################
+def square(n):
+    '''Takes a number and gives you square of the number'''
+    print(n**2)
+square(6)  
+print(square.__doc__)
