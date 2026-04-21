1e5a44451483c4ddda60d8f278340c9fec4c05a9
Harshith <sharshith241@gmail.com>
2026-04-21 06:42:34 +0530

    day_33

---
+++ day_33.py
@@ -0,0 +1,25 @@
+# dic={ "Harry":"Human Being","spoon":"object", "apartment":"building","Truth":True}
+# print(dic["apartment"])
+###################################################################################################
+# dic={ "Harry":"Human Being","spoon":"object", "apartment":"building","Truth":True}
+# print(dic["apartment"])
+# print(dic.get("Harry"))
+# print(dic["apartment"])
+# print(dic.values())
+# print(dic.keys())
+###################################################################################################
+# dic={ "Harry":"Human Being","spoon":"object", "apartment":"building","Truth":True}
+
+# for key in dic.keys():
+#     print(dic[key])
+###################################################################################################
+# dic={ "Harry":"Human Being","spoon":"object", "apartment":"building","Truth":True}
+# print(dic.items())
+###################################################################################################
+dic={ "Harry":"Human Being","spoon":"object", "apartment":"building","Truth":True}
+print(dic.items())
+for key,value in dic.items():
+   
+     
+     
+      print( f"The value corresponding to the key {key} is {value}") 
