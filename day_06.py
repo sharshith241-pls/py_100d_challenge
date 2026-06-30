4f1fd914c808cdf86fd4a0ce70722e5157f67cbe
sharshith241-pls <sharshith241@gmail.com>
2026-06-30 13:13:07 +0530

    day6

---
+++ day_06.py
@@ -0,0 +1,5 @@
+#variable-store somevariable
+#datatype-type of value the variable holds
+#list-ordered  collection of diff data
+#tuple-ordered immutable collection of data
+#dictionary-collection of key value pair
